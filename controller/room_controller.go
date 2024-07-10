852f54e3101f67fa687257518fcdf14d29c2c0c0
Desi Permata sari <desipermatasari1357@gmail.com>
2024-07-10 21:43:41 +0700

    NEW: add new feature get all, update and delete in room controller

--- controller/room_controller.go
+++ controller/room_controller.go
@@ -23,7 +23,10 @@ func (r *RoomController) Route() {
 	group.POST("/", r.createRoom)
 	group.GET("/:id", r.getRoomByID)
 	group.GET("/availability/:avail", r.getRoomByAvailability)
-	group.GET("/", r.getRoomByPriceLowerThanOrEqual)
+	group.GET("/budget/", r.getRoomByPriceLowerThanOrEqual)
+	group.GET("/rooms", r.getAllRooms)
+	group.PUT("/:id", r.updateRoom)
+	group.DELETE("/:id", r.deleteRoom)
 }
 
 func (r *RoomController) createRoom(ctx *gin.Context) {
@@ -41,6 +44,47 @@ func (r *RoomController) createRoom(ctx *gin.Context) {
 	}
 
 	util.SendSingleResponse(ctx, http.StatusCreated, "Success", room)
+}
+
+func (r *RoomController) updateRoom(ctx *gin.Context) {
+	var request dto.RoomRequest
+	err := ctx.ShouldBindJSON(&request)
+	if err != nil {
+		util.SendErrResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Param("id")
+	room, err := r.service.GetRoomByID(id)
+	if err != nil {
+		util.SendErrResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	room.KosID = request.KosID
+	room.Name = request.Name
+	room.Type = request.Type
+	room.Description = request.Description
+	room.Avail = request.Avail
+	room.Price = request.Price
+
+	updatedRoom, err := r.service.UpdateRoom(room)
+	if err != nil {
+		util.SendErrResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	util.SendSingleResponse(ctx, http.StatusOK, "Success", updatedRoom)
+}
+
+func (r *RoomController) getAllRooms(ctx *gin.Context) {
+	rooms, err := r.service.GetAllRooms()
+	if err != nil {
+		util.SendErrResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	util.SendSingleResponse(ctx, http.StatusOK, "Success", rooms)
 }
 
 func (r *RoomController) getRoomByID(ctx *gin.Context) {
@@ -79,4 +123,15 @@ func (r *RoomController) getRoomByPriceLowerThanOrEqual(ctx *gin.Context) {
 	}
 
 	util.SendSingleResponse(ctx, http.StatusOK, "Success", rooms)
+}
+
+func (r *RoomController) deleteRoom(ctx *gin.Context) {
+	id := ctx.Param("id")
+	err := r.service.DeleteRoom(id)
+	if err != nil {
+		util.SendErrResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	util.SendSingleResponse(ctx, http.StatusOK, "Success", nil)
 }
