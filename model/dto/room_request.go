a945d30288862fb3efcf0ca38313b93350e31df5
Desi Permata sari <desipermatasari1357@gmail.com>
2024-07-10 21:39:58 +0700

    UPDATE: update room request dto by adding kos id

--- model/dto/room_request.go
+++ model/dto/room_request.go
@@ -1,6 +1,7 @@
 package dto
 
 type RoomRequest struct {
+	KosID       string `json:"kos_id"`
 	Name        string `json:"name"`
 	Type        string `json:"type"`
 	Description string `json:"description"`
